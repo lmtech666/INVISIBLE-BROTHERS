a8e4db642725c4ec8d6febcb574120c60540f7c6
lmtech666 <01worldoftechnology@gmail.com>
2024-10-12 08:18:57 +0200

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "Q6chmYAK#tPzjk5ygFXAYqB8Pc0cTboRnMssTbzK0A4oCgcJzSt0",
 MONGODB: process.env.MONGODB || "mongodb://mongo:MfPJLADhRWNHvxuOORrlmfJHboedaADB@junction.proxy.rlwy.net:57828",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/70ssfa.jpg",
-ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi AI-MD V1.9 Is Online Now 💻\n*💻 Owner* - TCRONEB HACKX\n\n*💻 Owner Number* -263716394979",
+ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi ɪʙʙᴏᴛ-xᴍᴅ Is Online Now 💻\n*💻 Owner* - TCRONEB HACKX\n\n*💻 Owner Number* -263716394979",
 SUDO_NB: process.env.SUDO_NB || "263716394979",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
